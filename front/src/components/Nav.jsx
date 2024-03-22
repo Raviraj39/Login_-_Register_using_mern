fe7ccad0118c15827321198fa9750a22d5ece930
Raviraj39 <rravirajbujawade@gmail.com>
2024-03-23 01:52:05 +0530

    Nav Update

--- front/src/components/Nav.jsx
+++ front/src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as  Link } from 'react-router-dom'
 
 const Nav = () => {
   return (
